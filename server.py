0432dd2c0fdb3e802e49ce5e253bc9fc47921836
adnanirfan <adnanshurta@gmail.com>
2016-05-29 19:10:34 +0500

    added html pages, python server with restful api's

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/user/<name>")
+def hello(name=None):
+    if name is not None:
+        name = str.capitalize(name[0:1]) + name[1:10]
+        return render_template("index.html", name=name)
+    else:
+        return render_template("index.html")
+
+
+# Error Handlers
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html'), 404
+
+
+if __name__ == "__main__":
+    # app.debug = True
+    app.run('localhost', 3000, debug=True)
